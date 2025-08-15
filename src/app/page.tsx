eae4f316476686419c2d19f2a9fde4d3deed55bb
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2025-08-15 14:15:40 -0300

    global path fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 
 export default function Home() {
-  return{}
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello World
+      </h1>
+    </main>
+  )
 }
